5a07db68a1ec36525eb28087313a301728bf2baa
jdeboi <jdeboi@gmail.com>
2017-01-05 19:40:48 -0600

    renaming title

--- sketch.js
+++ sketch.js
@@ -1,14 +1,10 @@
-var x = 5;
-var y = 10;
-console.log(x + 5);     // 10
-console.log(x * 5);     // 25
-console.log(y / x);     // 2
+
+// Template project for p5
 
 function setup() {
-  createCanvas(600, 600);
+  
 }
 
 function draw() {
-  background(200);
-  ellipse(300, 200, 50, 50);
+
 }
